f3924b5bb56c4c9746406c401f8a306e8f79025a
Leighton Shank <shanklt@jmu.edu>
2015-07-15 17:46:25 -0400

    added replay feature
    added a command line option `--replay` that will replay the state "live"
    as it happened.  Updates will come in as they did during the actual live
    feed delayed by the appropriate time between events.  If the feed
    catches up to real-time then it will continue polling and displaying
    updates as usual.  During the replay, if the stage is still in progress,
    polling will continue and new events will be added to the queue.

--- index.js
+++ index.js
@@ -1,61 +1,23 @@
-var request = require('request')
-  , colors = require('colors')
-  , tdfLive = require('./lib/tdf-live');
+var TdfLive = require('./lib/tdf-live'),
+    colors = require('colors'),
+    minimist = require('minimist');
 
-// Get the main page, with all of the updates
-request.get('http://live.cyclingnews.com', function(err, res, body) {
-  // get out if we couldn't connect to the main page
-  if (err || res.statusCode !== 200) {
-    console.error("Unable to connect to the Cyclingnews live feed!");
-    if (err) {
-      console.error(err);
-    }
+var args = minimist(process.argv.slice(2));
+
+var options = {
+  feedUrl: args.feedUrl || 'http://live.cyclingnews.com',
+  pollUrl: args.pollUrl || 'http://live.cyclingnews.com/live/entries',
+  replay: args.replay || false
+}
+
+var tdfLive = new TdfLive(options);
+
+tdfLive.start(function(err) {
+  if (err) {
+    console.error(err);
     process.exit(1);
   }
 
-  // parse feed and display
-  tdfLive.parse(body).forEach(tdfLive.display);
-
-  // if we're done then say bye and be done
-  if (tdfLive.meta.complete) {
-    console.log("Live feed is done for today.  Check back tomorrow!".blue);
-    process.exit(1);
-  }
-
-  // if we're still going then make a request to the entries endpoint
-  // and update whenever we get a response
-  var poll = function() {
-    return request.get('http://live.cyclingnews.com/live/entries')
-    .on('response', function(res) {
-      if (res.statusCode == 200) {
-        var body = '';
-        res.on('data', function(chunk) {
-          body += chunk;
-        })
-        .on('end', function() {
-          var data = JSON.parse(body);
-          var update = {
-            distance: data.distance,
-            text: tdfLive.build(data.body),
-            timestamp: tdfLive.timestamp(data.modified)
-          };
-
-          tdfLive.display(update);
-          poll();
-        });
-      }
-      else {
-        // if we didn't get a 200 response (most likely a 500),
-        // then just keep polling
-        poll();
-      }
-    })
-    .on('error', function(err) {
-      // if we received an error just poll again
-      poll();
-    });
-  };
-
-  // start polling
-  poll();
+  console.log("Live feed is done for today.  Check back tomorrow!".blue);
+  process.exit(0);
 });
